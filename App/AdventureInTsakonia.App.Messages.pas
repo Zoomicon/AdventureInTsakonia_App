c932800bd6987092de6befdc5d47035a38e80ca0
birbilis <birbilis@zoomicon.com>
2026-01-06 22:56:32 +0200

    - using READCOM.Core.Delphi 18.0 - using new Default page with Poster image

--- App/AdventureInTsakonia.App.Messages.pas
+++ App/AdventureInTsakonia.App.Messages.pas
@@ -9,8 +9,11 @@ implementation
   uses READCOM.App.Messages;
 
 initialization
-  URL_HELP := 'https://github.com/Zoomicon/AdventureInTsakonia_App/wiki';
-  URL_READCOM := 'https://tsakonianarchives.codable.gr';
   STR_APP_TITLE := 'AdventureInTsakonia';
+
+  URL_HELP := 'https://github.com/Zoomicon/AdventureInTsakonia_App/wiki';
+  URL_READCOM := 'https://TsakonianArchives.gr';
+
+  URL_GALLERY := 'https://raw.githubusercontent.com/Zoomicon/AdventureInTsakonia_Gallery/master/Gallery/';
 
 end.
